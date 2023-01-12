9e7c6c361a1b3c268838c45a807ba2a9226b52db
Prasanna <kcgnanaprasanna99@gmail.com>
2023-01-12 20:56:05 +0530

    new feature added

--- javaprogram1.java
+++ javaprogram1.java
@@ -1,8 +1,10 @@
-class helloworld
+lass helloworld
 {    
       public static void main(String args[])
 	  {
 	    System.out.println("welcome to devOps")
+		System.out.println("welcome to day2 !)
+		
 		}
 }
 		
